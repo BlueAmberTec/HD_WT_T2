ff0ba7ba8804db08ce37aef069613d8f2c4ce6cb
blueAmer <wzq_steady@126.com>
2016-06-02 10:30:39 +0800

    add a new class

---
+++ app/src/main/java/com/hengda/smart/wt/BleConsoleActivity.java
@@ -0,0 +1,13 @@
+package com.hengda.smart.wt;
+
+/**
+ * @author WZQ
+ * @version V1.0
+ * @Description ${蓝牙测试类}
+ * @Email :wzq_steady@126.com
+ * @date 2016/6/2 10:28
+ * @update (date)
+ */
+public class BleConsoleActivity {
+
+}
